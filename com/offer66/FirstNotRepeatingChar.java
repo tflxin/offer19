06fde0411e6ca71dc8ed10bad9012875f6d47188
tfl xin <1445971703@qq.com>
2018-08-21 11:40:01 +0800

    数组中逆序对数

--- com/offer66/FirstNotRepeatingChar.java
+++ com/offer66/FirstNotRepeatingChar.java
@@ -13,30 +13,28 @@ public class FirstNotRepeatingChar
     public int FirstNotRepeatingChar(String str)
     {
 
-        HashMap<Character,Integer> map=new HashMap<Character,Integer>();
-        for(int i=0;i<str.length();i++)
-        {
-            char c=str.charAt(i);
-            if(map.containsKey(c))
-            {
-                int time=map.get(c);
+        HashMap<Character,Integer> map = new HashMap<Character, Integer>();
+        for (int i=0;i<str.length();i++){
+            char c = str.charAt(i);
+            if (map.containsKey(c)){
+                int time = map.get(c);
                 time++;
                 map.put(c,time);
             }
-            else
-            {
+            else{
                 map.put(c,1);
             }
         }
-        for(int i=0;i<str.length();i++)
-        {
+        for (int i=0;i<str.length();i++){
             char c=str.charAt(i);
-            if(map.get(c)==1)
+            if(map.get(c)==1){
                 return i;
+            }
         }
         return -1;
     }
 }
+
 
 /**
  public class Solution {
