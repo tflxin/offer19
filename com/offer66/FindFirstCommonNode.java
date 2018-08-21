b7cb16dd0807757db81044a79d191d618032b843
tfl xin <1445971703@qq.com>
2018-08-21 11:39:15 +0800

    两个链表第一个公共节点

---
+++ com/offer66/FindFirstCommonNode.java
@@ -0,0 +1,46 @@
+package com.offer66;
+
+public class FindFirstCommonNode {
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+       if(pHead1==null||pHead2 == null){
+           return null;
+       }
+       int count1 = 0;
+       ListNode p1 = pHead1;
+       while(p1!=null){
+           p1 = p1.next;
+           count1++;
+       }
+       int count2 = 0;
+       ListNode p2 = pHead2;
+       while(p2!=null){
+           p2=p2.next;
+           count2++;
+       }
+       int flag = count1 - count2;
+       if (flag>0){
+           while(flag>0){
+               pHead1 = pHead1.next;
+               flag--;
+           }
+           while(pHead1!=pHead2){
+               pHead1 = pHead1.next;
+               pHead2 = pHead2.next;
+           }
+           return pHead1;
+       }
+
+       if(flag<=0){
+           while(flag<0){
+               pHead2=pHead2.next;
+               flag++;
+           }
+           while (pHead1 != pHead2){
+               pHead2 = pHead2.next;
+               pHead1 = pHead1.next;
+           }
+           return pHead1;
+       }
+        return null;
+    }
+}
