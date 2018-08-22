9b60ff41fb34e3196b80f8b3fa4604e1c9b026fb
tfl xin <1445971703@qq.com>
2018-08-22 20:58:27 +0800

    左旋转字符串

---
+++ com/offer66/LeftRotateString.java
@@ -0,0 +1,50 @@
+package com.offer66;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.lang.StringBuffer;
+import java.util.Scanner;
+
+public class LeftRotateString {
+    public static String LeftRotateString(String str,int n) {
+
+        int length = str.length();
+        if(length<=0){
+            return "";
+        }
+        StringBuffer sb = new StringBuffer(str.substring(0, n));
+        StringBuffer sb1 = new StringBuffer(str.substring(n, str.length()));
+        sb1.append(sb);
+        return sb1.toString();
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入数字n");
+        int n =sc.nextInt();
+        StringBuffer str = new StringBuffer();
+        BufferedInputStream buff = new BufferedInputStream(System.in);
+
+        //BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("请输入字符");
+
+        char inChar;
+        try {
+            int in = buff.read();
+            while (in != -1 && in != '\n') {
+                inChar = (char)in;
+                str.append(inChar);
+                in = buff.read();
+            }
+            buff.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(str.toString());
+
+        System.out.println(LeftRotateString(str.toString(),n));
+
+    }
+    }
+
