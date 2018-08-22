98f4e0c5b32b6ca58c9f6bdf5a1188afcc9a63bf
tfl xin <1445971703@qq.com>
2018-08-22 21:01:58 +0800

    和为S的两个数

--- com/offer66/FindNumbersWithSum.java
+++ com/offer66/FindNumbersWithSum.java
@@ -56,7 +56,7 @@ public class FindNumbersWithSum {
         for (int i=0;i<array.length;i++){
             System.out.print(array[i] + " ");
         }
-        
+
         System.out.println(FindNumberWithSum(array,sum));
 
     }
