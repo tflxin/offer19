b07eeb820de4814be9ad02b22698b311bdc40697
tfl xin <1445971703@qq.com>
2018-08-24 09:54:43 +0800

    表示数值的字符串

--- com/offer66/isNumeric.java
+++ com/offer66/isNumeric.java
@@ -20,41 +20,63 @@ package com.offer66;
  }
  }
  */
+
+import java.util.Scanner;
+
+/**
+ * 【解题思路】
+ * //1. 合法的数值可以表示为A[.[B]][e|EC]或者.B[e|EC].
+ * //2. 其中A为整数部分，B为小数部分，C为e|E的指数部分。在纯小数的时候，
+ * 可能没有整数部分，如小数.123等于0.123，是合法的。所以A部分不是必须的。
+ * //3. A和C都是整数，可以带符号，也可不带。B是一个无符号整数。
+ */
 //剑指offer 版本 20ms,9288
 public class isNumeric {
-    private int index = 0;
+    public static boolean isNumeric_1(char[] str){
+        //标记符号，小数点，e 是否出现过
+       if (str==null||str.length==0){
+           return false;
+       }
+        boolean sign = false;
+        boolean decimal = false;
+        boolean hsE = false;
+        for(int i = 0;i<str.length;i++){
+           //判断是否含有 e
+            if (str[i]=='e'||str[i]=='E'){
+                if(i==str.length-1){ return false;}
+                if (hsE) {  return false;}
+                hsE = true;
+            } else if (str[i]=='+'||str[i]=='-'){
+                //第二次出现必须在e后面
+                if (sign & str[i-1]!='e'&&str[i-1]!='E'){return false;}
+                //第一次出现+-而且不在开头
+                if (!sign &&i>0&& str[i-1]!='e'&&str[i-1]!='E'){return false;}
 
-    public boolean isNumeric(char[] str) {
-        if (str.length < 1)
-            return false;
-
-        boolean flag = scanInteger(str);
-
-        if (index < str.length && str[index] == '.') {
-            index++;
-            flag = scanUnsignedInteger(str) || flag;
+                sign = true;
+            } else if (str[i]=='.'){
+                //e后面是不可以有的(如果hsE或者已有.)
+                if (hsE||decimal) {return false;}
+                decimal = true;
+            } else if (str[i]<'0'||str[i]>'9'){return false; }
+              //前面所有均不满足，是的了
+               return true;
         }
+        //所有的位置都判断完了
+        return true;
+    }
+    public static void main(String[] args) {
+      try{
+          Scanner in = new Scanner(System.in);
+          System.out.println("请输入你的信息：");
+          String line = in.nextLine();
+          System.out.println("信息："+line);
+          in.close();
 
-        if (index < str.length && (str[index] == 'E' || str[index] == 'e')) {
-            index++;
-            flag = flag && scanInteger(str);
-        }
-
-        return flag && index == str.length;
+          System.out.println(isNumeric_1(line.toCharArray()));
+      }catch(Exception e){
+          e.printStackTrace();
+      }
 
     }
 
-    private boolean scanInteger(char[] str) {
-        if (index < str.length && (str[index] == '+' || str[index] == '-') )
-            index++;
-        return scanUnsignedInteger(str);
-
-    }
-
-    private boolean scanUnsignedInteger(char[] str) {
-        int start = index;
-        while (index < str.length && str[index] >= '0' && str[index] <= '9')
-            index++;
-        return start < index; //是否存在整数
-    }
 }
