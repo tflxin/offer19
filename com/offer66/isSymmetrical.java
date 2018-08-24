c7597fbfbfa86b788b5dbc98491d315bd0ff309a
tfl xin <1445971703@qq.com>
2018-08-24 17:48:40 +0800

    是否为镜像的二叉树

---
+++ com/offer66/isSymmetrical.java
@@ -0,0 +1,25 @@
+package com.offer66;
+
+public class isSymmetrical {
+   public boolean isSymmetrical(TreeNode pRoot){
+       if (pRoot == null){
+           return true;
+       }
+       return isSymmertricalHelper(pRoot.left,pRoot.right);
+   }
+
+    private boolean isSymmertricalHelper(TreeNode leftNode, TreeNode rightNode) {
+     if(leftNode == null){
+         return rightNode==null;
+     }
+     if (rightNode==null){
+         return false;
+     }
+     if (leftNode.val == rightNode.val){
+         return isSymmertricalHelper(leftNode.left,rightNode.right)
+                 && isSymmertricalHelper(leftNode.right,rightNode.left)
+     }
+     return false;
+   }
+
+}
